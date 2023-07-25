29eab04c5f3ecb76892c65dc3a84411d1c5a6d02
Manikandan S <56933033+manikandan-sanmugam@users.noreply.github.com>
2023-07-25 15:15:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('redbutton').addEventListener('click', function(){
+    document.querySelector('#carimage').src="/images/redcar.jpg";
+});
+document.getElementById('bluebutton').addEventListener('click', function(){
+    document.querySelector('#carimage').src="/images/bluecar.jpg";
+});
+document.getElementById('greybutton').addEventListener('click', function(){
+    document.querySelector('#carimage').src="/images/greycar.jpg";
+});
+document.getElementById('addtocartbutton').addEventListener('click', function(){
+    document.querySelector('#addtocartbutton h4').innerHTML = 'Your item is added to cart'
+});
