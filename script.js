121b45ef48dc6e835094a8bdcfb3313d596c24bb
Manikandan S <56933033+manikandan-sanmugam@users.noreply.github.com>
2023-07-25 15:19:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.getElementById('redbutton').addEventListener('click', function(){
-    document.querySelector('#carimage').src="/images/redcar.jpg";
+    document.querySelector('#carimage').src="images/redcar.jpg";
 });
 document.getElementById('bluebutton').addEventListener('click', function(){
-    document.querySelector('#carimage').src="/images/bluecar.jpg";
+    document.querySelector('#carimage').src="images/bluecar.jpg";
 });
 document.getElementById('greybutton').addEventListener('click', function(){
-    document.querySelector('#carimage').src="/images/greycar.jpg";
+    document.querySelector('#carimage').src="images/greycar.jpg";
 });
 document.getElementById('addtocartbutton').addEventListener('click', function(){
     document.querySelector('#addtocartbutton h4').innerHTML = 'Your item is added to cart'
